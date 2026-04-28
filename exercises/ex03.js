a1c12aec536e0223dab4e059eabf944283291def
RobertCooks4 <robertwohlfordjr@gmail.com>
2026-04-28 12:37:12 -0700

    Lab 4 exercise completion

--- exercises/ex03.js
+++ exercises/ex03.js
@@ -4,3 +4,14 @@ $("#needy-button") .click( function() {
     count = count + 1;
     $("#needy-button") .html("You clicked me " + count + " times");
 });
+let objectname = "energy drink";
+let objectcolor = "white";
+let objectage = 10;
+let isuseful = true;
+let moods = ["tired", "excited", "confident", "angry"];
+let energizedThing = {
+    name: "Energy Drink",
+    species: "Canned Beverage",
+    age: 10,
+    isDelicious: true
+};
