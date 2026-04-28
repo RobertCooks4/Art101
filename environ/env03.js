34d66871c57c7ccaaa70007235bd63e432df75d4
RobertCooks4 <robertwohlfordjr@gmail.com>
2026-04-28 13:10:15 -0700

    started work on environment 03

--- environ/env03.js
+++ environ/env03.js
@@ -1,6 +1,11 @@
+let environmentTitle = "The Maze of Cheese";
+
+let environmentElements = ["rats", "cheese", "eggs", "death"]
 
 let count=0;
 $("#cheese-button") .click( function() {
     count = count + 1;
     $("#cheese-button") .html("You Collected " + count + " cheese!");
+    Image(rat);
 });
+
