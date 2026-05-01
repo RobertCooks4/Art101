5d0e0998f8d0a290a414d1d692bdfdb85cbe68f9
RobertCooks4 <robertwohlfordjr@gmail.com>
2026-05-01 16:54:22 -0700

    Button

--- environ/env03.js
+++ environ/env03.js
@@ -6,6 +6,7 @@ let count=0;
 $("#cheese-button") .click( function() {
     count = count + 1;
     $("#cheese-button") .html("You Collected " + count + " cheese!");
-    Image(rat);
+    message = message + "<p>Great! Now bring that cheese to Swiss Steve!</p>";
+    $("#output").html(message);
 });
 
